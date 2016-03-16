dadc26995912d33a7a7e0732caf741186d072dec
Brian <brianflaherty64@gmail.com>
2016-03-16 14:50:11 -0400

    added flexbox layout and API call to Rotten Tomato

--- index.ios.js
+++ index.ios.js
@@ -16,17 +16,62 @@ var MOCKED_MOVIES_DATA = [
   {title: 'Amelie', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
+const REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 class AwesomeProject extends Component {
+  
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+  
+  constructor(props) {
+    super(props); 
+    this.state = {
+      movies: null,
+    };
+  }
+  
+  componentDidMount() {
+    this.fetchData();
+  }
+  
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-        <Text> {movie.title} </Text>
-        <Text> {movie.year} </Text>
-        <Image 
-          source={{uri: movie.posters.thumbnail}} 
-          style={styles.thumbnail} 
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
         />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -35,6 +80,7 @@ class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -42,6 +88,17 @@ const styles = StyleSheet.create({
   thumbnail: {
     width: 53,
     height: 81,
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center'    
   }
 });
 
